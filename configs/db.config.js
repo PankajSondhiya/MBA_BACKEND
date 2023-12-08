ec4e7d0af3934ef35d534f1931ab200bd93e524a
pankaj sondhiya <pankajsondhiya780@gamil.com>
2023-12-08 15:05:34 +0530

    final commit

---
+++ configs/db.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  DB_NAME: "mba_db",
+  DB_URL:
+    process.env.MONGODB_URI ||
+    `mongodb+srv://${process.env.USERNAME_1}:${process.env.PASSWORD}@cluster0.1gsmies.mongodb.net/mba_db`,
+};
---
+++ models/movie.model.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+
+const movieSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    casts: {
+        type: [String],
+        required: true,
+    },
+    trailerUrl: {
+        type: String,
+        required: true,
+    },
+    posterUrl: {
+        type: String,
+        required: true,
+    },
+    language: {
+        type: [String],
+        required: true,
+    },
+    genre: {
+        type: String,
+    },
+    director: {
+        type: String,
+    },
+    ratings: {
+        type: Number,
+    },
+    releaseStatus: {
+        type: String,
+        required: true,
+        default: "RELEASED",
+    },
+    releaseDate: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: () => Date.now(),
+        immutable: true,
+    },
+    updatedAt: {
+        type: Date,
+        default: () => Date.now(),
+    },
+});
+
+const Movie = mongoose.model("Movie", movieSchema);
+
+module.exports = Movie;
